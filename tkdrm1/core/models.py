63cb30ffdbfa5fd7fee31f88112bddfaf2bb8ec3
Dmitry <d77s@inbox.ru>
2024-12-11 16:02:55 +0300

    Next

--- tkdrm1/core/models.py
+++ tkdrm1/core/models.py
@@ -5,7 +5,7 @@ from django.core.validators import (MinValueValidator, MaxValueValidator,
 from django.db import models
 
 
-class Keepers(models.Model):
+class Rtu(models.Model):
     """."""
 
     code = models.IntegerField(
@@ -21,13 +21,69 @@ class Keepers(models.Model):
     )
     level = models.IntegerField(validators=[
         MinValueValidator(1),
-        MaxValueValidator(3)
-        ])
-    upper_id = models.ForeignKey(to="Keepers",
-                                 null=True,
-                                 blank=False,
-                                 on_delete=models.RESTRICT,
-                                 related_name="keepers_from_prev")
+        MaxValueValidator(3)],
+        default=1
+        )
+
+    def __str__(self):
+        """."""
+        return self.name
+
+
+class CustHouse(models.Model):
+    """."""
+
+    code = models.IntegerField(
+        unique=True,
+        null=False,
+        blank=False,
+        validators=[RegexValidator(regex=r'^1\d{8}$')]
+    )
+    title = models.CharField(
+        max_length=255,
+        unique=True,
+        null=False
+    )
+    level = models.IntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(3)],
+        default=2
+        )
+    rtu_id = models.ForeignKey(to=Rtu,
+                               null=True,
+                               blank=False,
+                               on_delete=models.RESTRICT,
+                               related_name="cust_house_to_rtu")
+
+    def __str__(self):
+        """."""
+        return self.name
+
+
+class CustPost(models.Model):
+    """."""
+
+    code = models.IntegerField(
+        unique=True,
+        null=False,
+        blank=False,
+        validators=[RegexValidator(regex=r'^1\d{8}$')]
+    )
+    title = models.CharField(
+        max_length=255,
+        unique=True,
+        null=False
+    )
+    level = models.IntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(3)],
+        default=3
+        )
+    cust_house_id = models.ForeignKey(to=CustHouse,
+                                      null=True,
+                                      blank=False,
+                                      on_delete=models.RESTRICT,
+                                      related_name="cust_post_to_cust_house")
 
     def __str__(self):
         """."""
