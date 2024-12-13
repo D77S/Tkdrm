3fa15b25880ca6b342a63b802ef23b60ba744568
Дмитрий <d77s@inbox.ru>
2024-12-13 22:27:50 +0300

    next

--- tkdrm1/core/management/commands/import1.py
+++ tkdrm1/core/management/commands/import1.py
@@ -1,56 +1,56 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+import math
 import pandas
+
+from core.models import Rtu, CustHouse, CustPost
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        # Чтение из эксель-файла в пандасовский тип данных.
-        # Его можно потом имтерировать двумерным образом.
-        data=pandas.read_excel('BD_25-11-2024.xlsx',
-                               # skiprows=0,
-                               # nrows=2,
-                               header=None,
-                               sheet_name='Новая база2',
-                               usecols=range(0, 17),
-                               )
+        data = pandas.read_excel('BD_25-11-2024.xlsx',
+                                 # skiprows=0,
+                                 # nrows=2,
+                                 header=None,
+                                 sheet_name='Новая база2',
+                                 usecols=range(0, 17),
+                                 )
 
-        # Пример кусочка полученных данных:
-        print('Пример кусочка данных:')
-        print(data.values[10])
+        clean_data = [['' if isinstance(j, float) and math.isnan(j) else str(j) for j in i] for i in data.values if isinstance(i[0], int)]  # noqa
 
-        # Преобразование в нормальный тип данных, список списков.
-        # Заодно заменяем все "пустые ячейки" на ''.
-        clean_data = [['' if str(j) == 'nan' else str(j) for j in i] for i in data.values if isinstance(i[0], int)]
-
-        # Заменяем некоторые сокращения на полные их названия.
         clean_clean_data = []
         for i in clean_data:
             clean_clean_data_row = []
             for j in i:
-                if j =='ДВТУ':
-                    clean_clean_data_row.append('Дальневосточное таможенное управление')
+                if j == 'ДВТУ':
+                    clean_clean_data_row.append('Дальневосточное таможенное управление')  # noqa
                 elif j == 'ПТУ':
-                    clean_clean_data_row.append('Приволжское таможенное управление')
+                    clean_clean_data_row.append('Приволжское таможенное управление')  # noqa
                 elif j == 'СТУ':
-                    clean_clean_data_row.append('Сибирское таможенное управление')
+                    clean_clean_data_row.append('Сибирское таможенное управление')  # noqa
                 elif j == 'СЗТУ':
-                    clean_clean_data_row.append('Северо-Западное таможенное управление')
+                    clean_clean_data_row.append('Северо-Западное таможенное управление')  # noqa
                 elif j == 'УТУ':
-                    clean_clean_data_row.append('Уральское таможенное управление')
+                    clean_clean_data_row.append('Уральское таможенное управление')  # noqa
                 elif j == 'ЦТУ':
-                    clean_clean_data_row.append('Центральное таможенное управление')
+                    clean_clean_data_row.append('Центральное таможенное управление')  # noqa
                 elif j == 'ЮТУ':
                     clean_clean_data_row.append('Южное таможенное управление')
                 elif j == 'СКТУ':
-                    clean_clean_data_row.append('Северо-Кавказское таможенное управление')
+                    clean_clean_data_row.append('Северо-Кавказское таможенное управление')  # noqa
                 else:
                     clean_clean_data_row.append(j)
             clean_clean_data.append(clean_clean_data_row)
-        
+
         del_flag = 'm'
-        while not(del_flag=='y' or del_flag=='n'):
+        while not (del_flag == 'y' or del_flag == 'n'):
             del_flag = input('Удолять старую БД (y/n)?')
         print(del_flag)
+
+        if del_flag == 'y':
+            CustPost.objects.all().delete()
+            CustHouse.objects.all().delete()
+            Rtu.objects.all().delete()
 
 
         print(clean_clean_data[0])
@@ -58,4 +58,3 @@ class Command(BaseCommand):
         print('...')
         print(clean_clean_data[len(clean_clean_data)-2])
         print(clean_clean_data[len(clean_clean_data)-1])
-                
