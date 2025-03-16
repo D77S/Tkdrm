d183a2f893f5dc3fa95f40408457f777b8cffc98
Дмитрий <d77s@inbox.ru>
2025-03-16 23:05:04 +0300

    next

--- tkdrm1/core/urls.py
+++ tkdrm1/core/urls.py
@@ -7,4 +7,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.all_list, name='all_list'),
+    path('<int:pk>/', views.dev_detail, name='dev_detail')
 ]
--- tkdrm1/core/views.py
+++ tkdrm1/core/views.py
@@ -1,14 +1,24 @@
 """."""
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
 from core.models import Device
 
 
 def all_list(request):
     """."""
     template_name = 'all_list.html'
-    all_dev_list = Device.objects.select_related('type')
-    print(all_dev_list[:10])
-    context = {
-        'all_dev_list': all_dev_list,
-    }
+    # all_dev_list = Device.objects.select_related('type')[:10]
+    all_dev_list = Device.objects.select_related().order_by('id')
+    paginator = Paginator(all_dev_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj,
+               'all_dev': all_dev_list}
+    return render(request, template_name, context)
+
+
+def dev_detail(request, pk):
+    """."""
+    template_name = 'dev_detail.html'
+    context = {'dev': get_object_or_404(Device, pk=pk)}
     return render(request, template_name, context)
