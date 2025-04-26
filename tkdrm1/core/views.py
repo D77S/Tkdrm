750a749846faea7156a2a0a527ad6ad6752c3991
Дмитрий <d77s@inbox.ru>
2025-04-27 00:28:41 +0300

    next

--- tkdrm1/core/views.py
+++ tkdrm1/core/views.py
@@ -65,7 +65,7 @@ def all_list(request: HttpRequest):
         'type__category',
     ).order_by('id')
     time_list = time_counter(time_list, 'Конец запроса из БД перечня приборов.')  # noqa
-    devs_dict = {item.id: {'temp': [], 'dev': item} for item in devs_qset}
+    devs_dict = {item.id: {'dev': item} for item in devs_qset}
 
     time_list = time_counter(time_list, 'Начало запроса из БД перечня reltodevs.')  # noqa
     reltodevs = RelToDev.objects.all().select_related(
@@ -73,25 +73,27 @@ def all_list(request: HttpRequest):
             'to_rel__cust_pl1__rtu',
             'to_rel__cust_pl1__custhouse__upper_id',
             'to_rel__cust_pl1__custpost__upper_id__upper_id'
-    )
+    ).order_by('-is_main_for_dev')
     time_list = time_counter(time_list, 'Конец запроса из БД перечня reltodevs.')  # noqa
 
     for reltodev in reltodevs:
-        dev = reltodev.to_dev
-        dev_id = dev.id
-        devs_dict[dev_id]['temp'].append(reltodev)
+        dev_id = reltodev.to_dev.id
+        if devs_dict[dev_id].get('main_found') is True:
+            continue
+        else:
+            devs_dict[dev_id].update(temp_main=reltodev,
+                                     main_found=reltodev.is_main_for_dev)
 
-    for dev_id in devs_dict:
-        devs_dict[dev_id]['temp'].sort(key=lambda x: not x.is_main_for_dev)
+    time_list = time_counter(time_list, 'Конец обсчёта всех reltodevs.')  # noqa
 
     temp_devs = []
 
     for dev_id in devs_dict:
-        temp = devs_dict[dev_id]['temp']
-        if len(temp) == 0:
+        temp = devs_dict[dev_id].get('temp_main')
+        if temp is None:
             site_of_usage = (None, None, None)
         else:
-            cpluse = temp[0].to_rel.cust_pl1
+            cpluse = temp.to_rel.cust_pl1
             if cpluse.rtu is not None:
                 site_of_usage = (cpluse.rtu, None, None)
             elif cpluse.custhouse is not None:
@@ -125,17 +127,17 @@ def all_list(request: HttpRequest):
         )
     )
 
+    time_list = time_counter(time_list, 'Конец сортировки всех мест эксплуатации.')  # noqa
+
     temp_devs2 = []
     for i, item in enumerate(temp_devs):
         temp_devs2.append((i + 1, item[0], item[1]))
-
-    print(f'temp_devs2[0]={temp_devs2[0]}')
 
     paginator = Paginator(temp_devs2, ALL_DEV_PAG)
     page_number = request.GET.get('page')
     curr_page_obj = paginator.get_page(page_number)
 
-    print(f'curr_page_obj.object_list={curr_page_obj.object_list}')
+    # print(f'curr_page_obj.object_list={curr_page_obj.object_list}')
 
     delta = time_list[-1:][0][0] - time_list[0][0]
     print(f'Всего заняло {delta}.')
